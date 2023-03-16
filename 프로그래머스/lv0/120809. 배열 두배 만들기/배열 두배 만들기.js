9e9876e5d317f4fb465118e005e83576144e1938
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-03-16 17:51:28 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.06 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,10 +1,3 @@
 function solution(numbers) {
-    let answer = [];
-    
-    for (let i=0; i<numbers.length; i++) {
-        let item = numbers[i];
-        answer.push(item*2);
-    }
-    
-    return answer;
+    return numbers.map(x=>x*2);
 }
