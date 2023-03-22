f2dd491c7f8566d543741661f5f35d8faa717fe4
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-03-22 21:33:14 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,12 +1,12 @@
 function solution(price) {
     var answer = 0;
     
-    if(price>=100000 && price<300000) {
-        return Math.floor(price * 0.95);
-    }else if(price>=300000 && price<500000) {
-        return Math.floor(price * 0.9);
-    }else if (price>=500000) {
-        return Math.floor(price * 0.8);
+    if(price >= 500000) {
+        answer = Math.floor(price * 0.8);
+    }else if(price >= 300000) {
+        answer = Math.floor(price * 0.9);
+    }else if(price >= 100000) {
+        answer = Math.floor(price * 0.95);
     }else {
         answer = price;
     }
