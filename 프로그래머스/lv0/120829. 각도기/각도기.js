1b3cc313a52a611e7b785f58689e52eb60b11e2c
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-03-27 01:42:06 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -0,0 +1,13 @@
+function solution(angle) {
+
+    if(angle === 180) {
+        return 4;
+    }else if(angle > 90 && angle < 180) {
+        return 3;
+    }else if(angle === 90) {
+        return 2;
+    }else {
+        return 1;
+    }
+
+}
