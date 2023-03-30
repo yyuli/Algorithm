d072569dcf4dea7dce939f010d34c58588d05065
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-03-30 18:03:44 +0900

    [level 0] Title: 순서쌍의 개수, Time: 4.78 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = 0;
+    
+    for (let i = 0; i<= n; i++) {
+        if(n % i === 0) {
+            answer += 1;
+        }else {
+            answer;
+        }
+    }
+    
+    return answer;
+}
