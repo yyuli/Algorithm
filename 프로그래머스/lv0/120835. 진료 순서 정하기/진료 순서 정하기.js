2054218db9b7ffee9a01ad0120186624db4319c4
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-04-01 00:03:58 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,19 +1,4 @@
 function solution(emergency) {
-    let newEmergency = [];
-    
-    for (let cnt = 0; cnt < emergency.length; cnt++) {
-        newEmergency.push({value: emergency[cnt], idx:cnt});  
-    };
-    newEmergency.sort((a,b) => b.value-a.value)
-    console.log(newEmergency);
-    
-    var answer = new Array(emergency.length);
-    
-    for (cnt = 0; cnt<newEmergency.length; cnt++) {
-        let item = newEmergency[cnt]
-        answer[item.idx] = cnt + 1;
-    }
-    
-    
-    return answer;
+    const arrayNew = [...emergency].sort((a,b)=>b-a);
+    return emergency.map(item => arrayNew.indexOf(item)+1);
 }
