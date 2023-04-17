0e998ddd03173292d7265dd2202c72e4dba5ac00
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-04-17 10:04:55 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,52 +1,10 @@
 function solution(numbers) {
-    var answer = 0;
-    let string = '';
+    let answer = '';
+    const numArray = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     
-    for (let i = 0;i<numbers.length;i+=1) {
-        string += numbers[i];
-        if (string.includes("one")) {
-            answer += "1";
-            string = '';
-        }
-        if (string.includes("two")) {
-            answer += "2";
-            string = '';
-        }
-        if (string.includes("three")) {
-            answer += "3";
-            string = '';
-        }
-        if (string.includes("four")) {
-            answer += "4";
-            string = '';
-        }
-        if (string.includes("zero")) {
-            answer += "0";
-            string = '';
-        }
-        if (string.includes("five")) {
-            answer += "5";
-            string = '';
-        }
-        if (string.includes("six")) {
-            answer += "6";
-            string = '';
-        }
-        if (string.includes("seven")) {
-            answer += "7";
-            string = '';
-        }
-        if (string.includes("eight")) {
-            answer += "8";
-            string = '';
-        }
-        if (string.includes("nine")) {
-            answer += "9";
-            string = '';
-        }
-        
-        
-        
-        };
-    return Number(answer);
+    numArray.forEach((nums, index) => {
+        numbers = numbers.replaceAll(nums, index);
+    });
+    
+    return parseInt(numbers);
 }
