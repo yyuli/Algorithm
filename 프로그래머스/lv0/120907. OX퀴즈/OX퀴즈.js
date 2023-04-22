d2df902dd9da6f09632f981f3c4a82f3c2c1914e
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-04-22 22:30:57 +0900

    [level 0] Title: OX퀴즈, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -1,44 +1,27 @@
 function solution(quiz) {
-    var answer = [];
-    let strTemp = String(quiz).split(/ |,/);
-    let result = 0;
-    let cnt = 4;
+    let answer = [];
+    let sum = 0;
     
-    for (let i = 0; i<strTemp.length; i++) {
-        let item = strTemp[i]
-        
-        if(item === "-") {
-            result = Number(strTemp[i-1]) - Number(strTemp[i+1]);
-            
-            if(result == strTemp[cnt]) {
+    for (let i = 0; i<quiz.length; i++) {
+        let arr = quiz[i].split(" ");
+        if(arr[1] === "-") {
+            sum = parseInt(arr[0]) - parseInt(arr[2]);
+            if(sum === parseInt(arr[4])) {
                 answer.push("O");
-                cnt += 5;
             }else {
                 answer.push("X");
-                cnt += 5;
             }
-            
-        }else if(item === "+") {
-            result = Number(strTemp[i-1]) + Number(strTemp[i+1]);
-            
-            if(result == strTemp[cnt]) {
+         }else if(arr[1] === "+") {
+            sum = parseInt(arr[0]) + parseInt(arr[2]);
+            if(sum === parseInt(arr[4])) {
                 answer.push("O");
-                cnt += 5;
             }else {
                 answer.push("X");
-                cnt += 5;
             }
-            
-            console.log(result);
+             
         }
-        
-
     }
     
-    console.log(strTemp);
-    console.log(strTemp.length);
-    console.log(result);
     
     return answer;
-    
 }
