5608fc95c565e61e94598825eb10911bbbd36114
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-04-26 00:51:10 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,43 +1,8 @@
 function solution(dots) {
-    var answer = 0;
-    let arr1 = [];
-    let arr2 = [];
-    let num = 0;
-    let num2 = 0;
+    dots.sort((a,b)=>a[0] - b[0]);
+
+    const x = Math.abs(dots[2][0] - dots[0][0]);
+    const y = Math.abs(dots[0][1] - dots[1][1]);
     
-    for (let i = 0; i<dots.length; i++) {
-        arr1.push(dots[i][0]);
-    }
-    
-    for (let i = 0; i<dots.length; i++) {
-        arr2.push(dots[i][1]);
-    }
-    
-    for (let i = 0; i<arr1.length; i++) {
-        for (let j = 1; j<arr1.length; j++) {
-            if(arr1[i] !== arr1[j] && arr1[i] > arr1[j]) {
-                num = arr1[i] - arr1[j];
-                break;
-            }else if (arr1[i] !== arr1[j] && arr1[i] < arr1[j]) {
-                num = arr1[j] - arr1[i];
-                break;
-            }
-        }
-    }
-    
-    for (let i = 0; i<arr2.length; i++) {
-        for (let j = 1; j<arr2.length; j++) {
-            if(arr2[i] !== arr2[j] && arr2[i] > arr2[j]) {
-                num2 = arr2[i] - arr2[j];
-                break;
-            }else if (arr2[i] !== arr2[j] && arr2[i] < arr2[j]) {
-                num2 = arr2[j] - arr2[i];
-                break;
-            }
-        }
-    }
-    
-    answer = num * num2;
-    
-    return answer;
+    return x*y;
 }
