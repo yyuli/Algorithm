c200a9af6ca0813f817981912aaea09351436e06
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-05-03 17:59:45 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,12 +1,13 @@
 function solution(n) {
-    let x3 = 0;
+    let answer = 0;
     
     for (let i = 1; i<=n; i++) {
-        x3++;
-        while(x3.toString().includes("3") || x3 % 3 === 0) {
-            x3++;
+        answer++;
+        while(answer.toString().includes("3") || answer % 3 === 0) {
+            answer++;
         }
     }
     
-    return x3;
+    return answer;
 }
+
