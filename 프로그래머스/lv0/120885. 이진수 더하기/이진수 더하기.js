f6363edbdb0623897057a8d7753e34af3955a319
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-05-10 22:20:20 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
@@ -1,5 +1,6 @@
 function solution(bin1, bin2) {
-    let answer = (parseInt(bin1,2) + parseInt(bin2,2)).toString(2);
+    const num1 = parseInt(bin1, 2);
+    const num2 = parseInt(bin2, 2)
     
-    return answer;
+    return (num1+num2).toString(2);
 }
